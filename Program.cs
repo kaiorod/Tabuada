34c3efb800cf5a674cb4bcfb8d87c198d48a1d3c
kaiorod <caiorodrigo2775@gmail.com>
2021-11-21 18:06:13 -0300

    Finaliza Tabuada

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int c, n, tab; 
+            c = 0;
+
+            Console.Write("Digite um número : ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            while(c<=10)
+            {
+                tab = n * c;
+
+                Console.WriteLine($"{n} X {c} = {tab}");
+
+                c = c + 1;
+            }
+        }
+    }
+}
